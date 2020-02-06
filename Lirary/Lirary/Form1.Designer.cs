9d2435897212ee799f412ab8b5b84e624d5c7006
parasvasoya <60727596+parasvasoya@users.noreply.github.com>
2020-02-06 12:30:39 +0530

    Add files via upload
    letest

--- Lirary/Lirary/Form1.Designer.cs
+++ Lirary/Lirary/Form1.Designer.cs
@@ -54,54 +54,62 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label1.Location = new System.Drawing.Point(38, 88);
+            this.label1.Location = new System.Drawing.Point(51, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.Size = new System.Drawing.Size(135, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "User Login";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(85, 134);
+            this.textBox1.Location = new System.Drawing.Point(113, 165);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 137);
+            this.label2.Location = new System.Drawing.Point(16, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.Size = new System.Drawing.Size(74, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Student ID";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(43, 167);
+            this.button1.Location = new System.Drawing.Point(57, 206);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 3;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label3.Location = new System.Drawing.Point(234, 88);
+            this.label3.Location = new System.Drawing.Point(312, 108);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(130, 25);
+            this.label3.Size = new System.Drawing.Size(165, 29);
             this.label3.TabIndex = 4;
             this.label3.Text = "Issued Books";
             // 
             // issued_books
             // 
             this.issued_books.AutoSize = true;
-            this.issued_books.Location = new System.Drawing.Point(236, 156);
+            this.issued_books.Location = new System.Drawing.Point(315, 192);
+            this.issued_books.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.issued_books.Name = "issued_books";
-            this.issued_books.Size = new System.Drawing.Size(131, 13);
+            this.issued_books.Size = new System.Drawing.Size(175, 17);
             this.issued_books.TabIndex = 5;
             this.issued_books.Text = "No book\'s currently issued";
             this.issued_books.Click += new System.EventHandler(this.issued_books_Click);
@@ -110,43 +118,49 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label4.Location = new System.Drawing.Point(461, 88);
+            this.label4.Location = new System.Drawing.Point(615, 108);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(130, 25);
+            this.label4.Size = new System.Drawing.Size(165, 29);
             this.label4.TabIndex = 6;
             this.label4.Text = "Issue Section";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(453, 137);
+            this.label5.Location = new System.Drawing.Point(604, 169);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.Size = new System.Drawing.Size(57, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "Book ID";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(523, 133);
+            this.textBox2.Location = new System.Drawing.Point(697, 164);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 8;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(466, 167);
+            this.button2.Location = new System.Drawing.Point(621, 206);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 9;
             this.button2.Text = "Issue";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(566, 167);
+            this.button3.Location = new System.Drawing.Point(755, 206);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 10;
             this.button3.Text = "Return";
             this.button3.UseVisualStyleBackColor = true;
@@ -155,9 +169,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label6.Location = new System.Drawing.Point(213, 19);
+            this.label6.Location = new System.Drawing.Point(284, 23);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(328, 39);
+            this.label6.Size = new System.Drawing.Size(402, 48);
             this.label6.TabIndex = 11;
             this.label6.Text = "Library Management";
             // 
@@ -165,27 +180,30 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label7.Location = new System.Drawing.Point(111, 295);
+            this.label7.Location = new System.Drawing.Point(163, 258);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(142, 25);
+            this.label7.Size = new System.Drawing.Size(182, 29);
             this.label7.TabIndex = 12;
             this.label7.Text = "Available Book";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(40, 334);
+            this.label8.Location = new System.Drawing.Point(68, 306);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.Size = new System.Drawing.Size(57, 17);
             this.label8.TabIndex = 13;
             this.label8.Text = "Book ID";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(113, 334);
+            this.label9.Location = new System.Drawing.Point(166, 306);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(63, 13);
+            this.label9.Size = new System.Drawing.Size(81, 17);
             this.label9.TabIndex = 14;
             this.label9.Text = "Book Name";
             this.label9.Click += new System.EventHandler(this.label9_Click);
@@ -193,9 +211,10 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(263, 334);
+            this.label10.Location = new System.Drawing.Point(366, 306);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(50, 13);
+            this.label10.Size = new System.Drawing.Size(67, 17);
             this.label10.TabIndex = 15;
             this.label10.Text = "Publisher";
             this.label10.Click += new System.EventHandler(this.label10_Click);
@@ -203,30 +222,34 @@
             // book_id
             // 
             this.book_id.AutoSize = true;
-            this.book_id.Location = new System.Drawing.Point(43, 360);
-            this.book_id.MinimumSize = new System.Drawing.Size(50, 0);
+            this.book_id.Location = new System.Drawing.Point(72, 338);
+            this.book_id.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.book_id.MinimumSize = new System.Drawing.Size(67, 0);
             this.book_id.Name = "book_id";
-            this.book_id.Size = new System.Drawing.Size(50, 13);
+            this.book_id.Size = new System.Drawing.Size(67, 17);
             this.book_id.TabIndex = 16;
             this.book_id.Text = "null";
+            this.book_id.Click += new System.EventHandler(this.book_id_Click);
             // 
             // book_name
             // 
             this.book_name.AutoSize = true;
-            this.book_name.Location = new System.Drawing.Point(116, 359);
-            this.book_name.MinimumSize = new System.Drawing.Size(130, 0);
+            this.book_name.Location = new System.Drawing.Point(170, 337);
+            this.book_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.book_name.MinimumSize = new System.Drawing.Size(173, 0);
             this.book_name.Name = "book_name";
-            this.book_name.Size = new System.Drawing.Size(130, 13);
+            this.book_name.Size = new System.Drawing.Size(173, 17);
             this.book_name.TabIndex = 17;
             this.book_name.Text = "null";
             // 
             // publisher
             // 
             this.publisher.AutoSize = true;
-            this.publisher.Location = new System.Drawing.Point(266, 360);
-            this.publisher.MinimumSize = new System.Drawing.Size(150, 0);
+            this.publisher.Location = new System.Drawing.Point(370, 338);
+            this.publisher.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.publisher.MinimumSize = new System.Drawing.Size(200, 0);
             this.publisher.Name = "publisher";
-            this.publisher.Size = new System.Drawing.Size(150, 13);
+            this.publisher.Size = new System.Drawing.Size(200, 17);
             this.publisher.TabIndex = 18;
             this.publisher.Text = "null";
             // 
@@ -234,17 +257,18 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(236, 133);
+            this.label11.Location = new System.Drawing.Point(315, 164);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(53, 13);
+            this.label11.Size = new System.Drawing.Size(64, 17);
             this.label11.TabIndex = 19;
             this.label11.Text = "Book ID";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.publisher);
             this.Controls.Add(this.book_name);
@@ -265,6 +289,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Lirary/Lirary/Form1.cs
+++ Lirary/Lirary/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Lirary
 {
     public partial class Form1 : Form
     {
+        int id;
+        OleDbConnection con;
+        OleDbCommand cmd;
+        OleDbDataReader Reader;
+
         public Form1()
         {
             InitializeComponent();
+            con = new OleDbConnection(@"Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\Users\Paras vasoya\Desktop\library_management.mdb");
+            con.Open();
+            cmd = new OleDbCommand();
+            cmd.Connection = con;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -45,6 +55,53 @@ namespace Lirary
         private void label10_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(textBox1.Text);
+            cmd.CommandText = "Select * from issued_record where s_id="+id;
+            Reader=cmd.ExecuteReader();
+            issued_books.Text = null;
+            while (Reader.Read())
+            {
+                issued_books.Text += Reader[1].ToString()+"\n";
+                
+            }
+            Reader.Close();
+            cmd.CommandText = "SELECT * FROM book WHERE count > 0";
+            Reader=cmd.ExecuteReader();
+            book_name.Text = null;
+            publisher.Text = null;
+            book_id.Text = null;
+            
+            while (Reader.Read())
+            {
+                book_id.Text += Reader[0].ToString()+"\n";
+                book_name.Text += Reader[1].ToString() + "\n";
+                publisher.Text += Reader[2].ToString() + "\n";
+            }
+            Reader.Close();
+          //  con.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void book_id_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(textBox1.Text);
+            int bid = Convert.ToInt32(textBox2.Text);
+            cmd.CommandText = "Insert into issued_record values(id,bid) select s_id,b_id from issued_record join book where book.count>0 ";
+            //Reader=cmd.ExecuteReader();
+            cmd.ExecuteNonQuery();
         }
     }
 }
